ac1d79082cb8b880d394145da2cef1b234fbc657
alstn113 <alstn113@gmail.com>
2024-11-19 19:23:24 +0900

    refactor: migrate to tailwind

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,8 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
-import GlobalStyles from './GlobalStyles';
+import { NextUIProvider } from '@nextui-org/react';
+import './index.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,7 +16,8 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <ReactQueryDevtools initialIsOpen={false} />
-    <GlobalStyles />
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </QueryClientProvider>,
 );

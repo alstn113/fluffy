d5579eed626e71a33a801ae6368a6c04bf76661f
alstn113 <alstn113@gmail.com>
2024-12-24 12:36:28 +0900

    refactor: remove spring mail

--- server/src/main/java/com/fluffy/external/email/SendEmailRequest.java
+++
@@ -1,5 +0,0 @@
-package com.fluffy.external.email;
-
-
-public record SendEmailRequest(String to, String subject, String body) {
-}
--- server/src/main/resources/db/migration/V2__update_description_length.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE exam
-ALTER COLUMN description TYPE TEXT;

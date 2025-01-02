8189abe5f223eec6360607ea36af07383c84af23
alstn113 <alstn113@gmail.com>
2025-01-02 17:19:02 +0900

    feat: question passage 필드 추가

--- web/src/api/questionAPI.ts
+++ web/src/api/questionAPI.ts
@@ -22,6 +22,7 @@ export type QuestionWithAnswersResponse =
 export interface QuestionBaseResponse {
   id: number;
   text: string;
+  passage: string;
   type: QuestionType;
 }
 
@@ -58,6 +59,7 @@ export interface QuestionOptionWithAnswersResponse extends QuestionOptionRespons
 
 export interface QuestionBaseRequest {
   text: string;
+  passage: string;
   type: QuestionType;
 }
 

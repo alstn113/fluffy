c2018883635d6ac60dbf0b167eb5d08cfa0b9939
alstn113 <alstn113@gmail.com>
2025-02-11 21:33:02 +0900

    feat: 메서드 테스트 후 cache 지우기

--- server/src/test/java/com/fluffy/support/cleaner/DatabaseCleaner.java
+++ server/src/test/java/com/fluffy/support/cleaner/DatabaseCleaner.java
@@ -4,6 +4,8 @@ package com.fluffy.support.cleaner;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 public class DatabaseCleaner {
@@ -11,10 +13,14 @@ public class DatabaseCleaner {
     @PersistenceContext
     private EntityManager em;
 
+    @Autowired
+    private RedisTemplate<?, ?> redisTemplate;
+
     @Transactional
     public void clear() {
         em.clear();
         truncate();
+        clearCache();
     }
 
     private void truncate() {
@@ -31,5 +37,9 @@ public class DatabaseCleaner {
                 """;
 
         return em.createNativeQuery(sql).getResultList();
+    }
+
+    private void clearCache() {
+        redisTemplate.getConnectionFactory().getConnection().flushDb();
     }
 }

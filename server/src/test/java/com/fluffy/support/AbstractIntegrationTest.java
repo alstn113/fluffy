7eede677ed3a9b8d04667444512dc6a553d0a737
alstn113 <alstn113@gmail.com>
2025-02-23 21:55:20 +0900

    refactor: ApplicationContextInitializer로 테스트 컨테이너 코드 분리

--- server/src/test/java/com/fluffy/support/AbstractIntegrationTest.java
+++ server/src/test/java/com/fluffy/support/AbstractIntegrationTest.java
@@ -1,38 +1,19 @@
 package com.fluffy.support;
 
 import com.fluffy.support.cleaner.DataClearExtension;
+import com.fluffy.support.testcontainers.PostgreSQLContainerInitializer;
+import com.fluffy.support.testcontainers.RedisContainerInitializer;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ExtendWith(DataClearExtension.class)
 @ActiveProfiles("test")
+@ContextConfiguration(initializers = {
+        RedisContainerInitializer.class,
+        PostgreSQLContainerInitializer.class
+})
 public abstract class AbstractIntegrationTest {
-
-    private static final PostgreSQLContainer<?> POSTGRESQL = new PostgreSQLContainer<>("postgres:16-alpine");
-    private static final GenericContainer<?> REDIS = new GenericContainer<>("redis:7-alpine")
-            .withExposedPorts(6379);
-
-    static {
-        POSTGRESQL.start();
-        REDIS.start();
-    }
-
-    @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) {
-        // PostgreSQL 설정
-        registry.add("spring.datasource.url", POSTGRESQL::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRESQL::getUsername);
-        registry.add("spring.datasource.password", POSTGRESQL::getPassword);
-
-        // Redis 설정
-        registry.add("spring.data.redis.host", REDIS::getHost);
-        registry.add("spring.data.redis.port", REDIS::getFirstMappedPort);
-        registry.add("spring.data.redis.ssl.enabled", () -> false);
-    }
 }

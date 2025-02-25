5d55e5e4d01490d30e99e2fbef841d88303b913f
alstn113 <alstn113@gmail.com>
2025-02-25 20:47:17 +0900

    feat: ExamImageServiceTest 테스트 코드 작성

--- server/src/test/java/com/fluffy/support/AbstractIntegrationTest.java
+++ server/src/test/java/com/fluffy/support/AbstractIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.fluffy.support;
 
+import com.fluffy.storage.application.StorageClient;
 import com.fluffy.support.cleaner.DataClearExtension;
 import com.fluffy.support.testcontainers.PostgreSQLContainerInitializer;
 import com.fluffy.support.testcontainers.RedisContainerInitializer;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -16,4 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
         PostgreSQLContainerInitializer.class
 })
 public abstract class AbstractIntegrationTest {
+
+    @MockBean
+    protected StorageClient storageClient;
 }

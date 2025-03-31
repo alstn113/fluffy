9bb834ca14cecc2d160fc0fbb3715626b5660850
alstn113 <alstn113@gmail.com>
2025-03-31 13:39:07 +0900

    test

--- server/src/main/kotlin/com/fluffy/infra/persistence/AuditableEntity.kt
+++ server/src/main/kotlin/com/fluffy/infra/persistence/AuditableEntity.kt
@@ -1,12 +1,11 @@
 package com.fluffy.infra.persistence
 
-import jakarta.persistence.Column
-import jakarta.persistence.EntityListeners
-import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
@@ -21,4 +20,15 @@ abstract class AuditableEntity {
     @Column(nullable = false, columnDefinition = "TIMESTAMP(6)")
     var updatedAt: LocalDateTime = LocalDateTime.MIN
         protected set
+
+    @PrePersist
+    fun prePersist() {
+        createdAt = createdAt.truncatedTo(ChronoUnit.MICROS)
+        updatedAt = updatedAt.truncatedTo(ChronoUnit.MICROS)
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updatedAt = updatedAt.truncatedTo(ChronoUnit.MICROS)
+    }
 }
